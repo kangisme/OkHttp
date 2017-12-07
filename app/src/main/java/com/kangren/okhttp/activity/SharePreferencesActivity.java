16b3c2a8b79a38b5fd009476ddf78449a630b4c2
kangren <909296146@qq.com>
2017-12-07 20:36:23 +0800

    更新SharedPreferences存储：Gson转化存储复杂对象 十大原则之一：数据类重写toString()方法

--- app/src/main/java/com/kangren/okhttp/activity/SharePreferencesActivity.java
+++ app/src/main/java/com/kangren/okhttp/activity/SharePreferencesActivity.java
@@ -21,6 +21,8 @@ import com.kangren.okhttp.R;
 public class SharePreferencesActivity extends Activity {
     private SharedPreferences sp;
 
+    private Gson gson;
+
     private TextView result;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,9 +34,39 @@ public class SharePreferencesActivity extends Activity {
     private void init() {
         sp = getSharedPreferences("shared_preferences", MODE_PRIVATE);
         result = (TextView) findViewById(R.id.text_view);
+        gson = new Gson();
+        SingleAndList();
+        complexObject();
+    }
+
+    /**
+     * 包含Object的Object
+     */
+    private void complexObject() {
+        People people = new People("小明", "男", 25);
+        Family family = new Family(people);
+        family.setAddress("上海市");
+        family.setTelephone("123456");
+        SharedPreferences.Editor editor = sp.edit();
+        String familyJson = gson.toJson(family);
+        editor.putString("family", familyJson);
+        editor.apply();
+        findViewById(R.id.read_family).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String readJson = sp.getString("family", null);
+                Family family1 = gson.fromJson(readJson, Family.class);
+                result.setText(family1.toString());
+            }
+        });
+    }
+
+    /**
+     * JSON存储读取Object和List<Object>
+     */
+    private void SingleAndList() {
         //单一对象
         People people = new People("kang", "男", 22);
-        final Gson gson = new Gson();
         String peopleJson = gson.toJson(people);
         SharedPreferences.Editor editor = sp.edit();
         editor.putString("people", peopleJson);
@@ -45,8 +77,7 @@ public class SharePreferencesActivity extends Activity {
                 if (readJson != null)
                 {
                     People readPeople = gson.fromJson(readJson, People.class);
-                    String text = "姓名:" + readPeople.name + "\n性别:" + readPeople.sex + "\n年龄:" + readPeople.age;
-                    result.setText(text);
+                    result.setText(readPeople.toString());
                 }
             }
         });
@@ -65,13 +96,38 @@ public class SharePreferencesActivity extends Activity {
                 if (readJson != null)
                 {
                     List<People> readPeoples = gson.fromJson(readJson, new TypeToken<List<People>>() {}.getType());
-                    String text = readPeoples.get(0).name + readPeoples.get(0).sex + readPeoples.get(0).age +
-                            "\n" + readPeoples.get(1).name + readPeoples.get(1).sex + readPeoples.get(1).age;
+                    String text = readPeoples.get(0).toString() + readPeoples.get(1).toString();
                     result.setText(text);
                 }
             }
         });
     }
+
+    private class Family{
+        public String address;
+        public String telephone;
+        public People people;
+
+        public Family(People people) {
+            this.people = people;
+        }
+
+        public void setAddress(String address)
+        {
+            this.address = address;
+        }
+
+        public void setTelephone(String telephone)
+        {
+            this.telephone = telephone;
+        }
+
+        @Override
+        public String toString() {
+            return people.toString() + "address:" + address + "telephone:" + telephone;
+        }
+    }
+
 
     private class People{
         public String name;
@@ -82,6 +138,11 @@ public class SharePreferencesActivity extends Activity {
             this.name = name;
             this.sex = sex;
             this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "name:" + name + "sex:" + sex + "age:" + age;
         }
     }
 }
