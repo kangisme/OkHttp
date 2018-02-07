2c235684244576874e9de61ab1b925a06397d1ea
kangren <909296146@qq.com>
2018-02-07 15:33:32 +0800

    DexClassLoader加载其它apk文件，调用其中的类方法

--- app/src/main/java/com/kangren/practice/FunctionActivity.java
+++ app/src/main/java/com/kangren/practice/FunctionActivity.java
@@ -1,9 +1,15 @@
 package com.kangren.practice;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 
 import com.kangren.practice.activity.FingerprintActivity;
@@ -16,6 +22,10 @@ import com.kangren.practice.activity.RxAndroidActivity;
 import com.kangren.practice.activity.SharePreferencesActivity;
 import com.kangren.practice.activity.SystemInfoActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import dalvik.system.DexClassLoader;
+
 /**
  * Created by kangren on 2017/12/2.
  */
@@ -25,64 +35,65 @@ public class FunctionActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_function);
-        init();
+        ButterKnife.bind(this);
     }
 
-    private void init() {
-        findViewById(R.id.list_view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(ListViewActivity.class);
-            }
-        });
-        findViewById(R.id.recycler_view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(RecyclerViewActivity.class);
-            }
-        });
-        findViewById(R.id.okhttp).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(OkhttpActivity.class);
-            }
-        });
-        findViewById(R.id.event_bus).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(EventBusActivity.class);
-            }
-        });
-        findViewById(R.id.rx_android).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(RxAndroidActivity.class);
-            }
-        });
-        findViewById(R.id.system_info).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(SystemInfoActivity.class);
-            }
-        });
-        findViewById(R.id.shared_preferences).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(SharePreferencesActivity.class);
-            }
-        });
-        findViewById(R.id.spannable).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(SpannableActivity.class);
-            }
-        });
-        findViewById(R.id.fingerprint).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goActivity(FingerprintActivity.class);
-            }
-        });
+    @OnClick(R.id.list_view) void listView()
+    {
+        goActivity(ListViewActivity.class);
+    }
+    @OnClick(R.id.recycler_view) void recycle()
+    {
+        goActivity(RecyclerViewActivity.class);
+    }
+    @OnClick(R.id.okhttp) void okhttp()
+    {
+        goActivity(OkhttpActivity.class);
+    }
+    @OnClick(R.id.event_bus) void eventBus()
+    {
+        goActivity(EventBusActivity.class);
+    }
+    @OnClick(R.id.rx_android) void rxAndroid()
+    {
+        goActivity(RxAndroidActivity.class);
+    }
+    @OnClick(R.id.system_info) void systemInfo()
+    {
+        goActivity(SystemInfoActivity.class);
+    }
+    @OnClick(R.id.shared_preferences) void shared()
+    {
+        goActivity(SharePreferencesActivity.class);
+    }
+    @OnClick(R.id.spannable) void spannable(){
+        goActivity(SpannableActivity.class);
+    }
+    @OnClick(R.id.fingerprint) void fingerprint()
+    {
+        goActivity(FingerprintActivity.class);
+    }
+    @OnClick(R.id.dexclassloader) void dexclassloader()
+    {
+        String apkPath = Environment.getExternalStorageDirectory() + "/hello.apk";
+        loadApk(apkPath);
+    }
+
+    /**
+     * DexClassLoader加载其它apk文件中的类
+     * @param apkPath apk文件路径
+     */
+    private void loadApk(String apkPath) {
+        File dexOpt = this.getDir("dexOpt", MODE_PRIVATE);
+        final DexClassLoader classloader = new DexClassLoader(apkPath, dexOpt.getAbsolutePath(), null, this.getClassLoader());
+        try {
+            Class<?> classToLoad = (Class<?>)classloader.loadClass("com.kang.classtobeload.ClassToBeLoad");
+            Object instance = classToLoad.newInstance();
+            Method method = classToLoad.getMethod("hello");
+            method.invoke(instance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void goActivity(Class activity)
